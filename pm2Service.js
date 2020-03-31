b9c49c078b113653e25e809da2cb68167da83ecb
Luis Carlos Galvão <lu.ca.oliveira41@gmail.com>
2020-03-31 17:15:43 -0400

    Symbol correction

--- pm2Service.js
+++ pm2Service.js
@@ -1,4 +1,8 @@
 const pm2 = require("pm2");
+const statusSymbol = {
+  online: "\u{2705}",
+  stopped: "\u{1F6AB}"
+};
 exports.list = callback => {
   pm2.list((err, list) => {
     if (err) {
@@ -8,11 +12,13 @@ exports.list = callback => {
   });
 };
 exports.processData = process => {
-  return `<pre>STATUS: ${process.pm2_env.status} \u{2705}\nNAME: ${
-    process.name
-  }\nID: ${process.pid}\nMEMORY: ${(process.monit.memory / 1024 / 1024).toFixed(
-    2
-  )}MB\nCPU:${process.monit.cpu} %\n--------------\n</pre>`;
+  return `<pre>STATUS: ${process.pm2_env.status}  ${
+    statusSymbol[process.pm2_env.status]
+  }\nNAME: ${process.name}\nID: ${process.pid}\nMEMORY: ${(
+    process.monit.memory /
+    1024 /
+    1024
+  ).toFixed(2)}MB\nCPU:${process.monit.cpu} %\n--------------\n</pre>`;
 };
 exports.restart = (processName, callback) => {
   pm2.restart(processName, (err, process) => {
